189154e222bfae0d68d5d30155ca57aca9f6155e
Stefan Crkvenjakov <stefancrkvenjakov1@gmail.com>
2024-05-02 14:39:37 +0200

    V1.1.2

--- plugin.main.php
+++ plugin.main.php
@@ -3,15 +3,16 @@
 use settings\settings;
 
 use admin\navbar;
+
 $metrics = new navbar;
 
 $settings = new settings;
 
 $name = 'awtMetrics';
-$version = "0.0.1";
-$src = PLUGINS.$name.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR;
-$enginePath = $src.'awtMetrics.php';
-$dependenciesPath = $src.'awtMetrics.class.php';
+$version = "1.2.0";
+$src = PLUGINS . $name . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
+$enginePath = $src . 'awtMetrics.php';
+$dependenciesPath = $src . 'awtMetrics.class.php';
 
 include_once $dependenciesPath;
 
@@ -19,38 +20,31 @@ addDependency('high', $name, $dependenciesPath, $version);
 
 addEngine($name, $enginePath, $version, 'start');
 
-$pluginPages['awtMetrics'] = PLUGINS.'awtMetrics'.DIRECTORY_SEPARATOR.'plugin.page.php';
+$pluginPages['awtMetrics'] = PLUGINS . 'awtMetrics' . DIRECTORY_SEPARATOR . 'plugin.page.php';
 
-if(function_exists('navbarLoader')) {
-    $location = HOSTNAME.'awt-content/plugins/awtMetrics/data/icons/';
+if (function_exists('navbarLoader')) {
+    $location = HOSTNAME . 'awt-content/plugins/awtMetrics/data/icons/';
 
-    $metrics->addItem(array('icon' => $location.'chart-line-solid.svg', 'name'=>'Metrics', 'link' => '?page=awtMetrics'));
+    $metrics->addItem(array('icon' => $location . 'chart-line-solid.svg', 'name' => 'Metrics', 'link' => HOSTNAME . 'awt-admin/?page=awtMetrics'));
 
     array_push($navbar, $metrics);
 }
 
-$collection = "Metrics Blocks";
-createBlockCollection($collection);
 
-$defaultPath = PLUGINS.$name.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."blocks".DIRECTORY_SEPARATOR;
 
-$tester = array("name" => "Metrics", "path" => $defaultPath."most-visited.php");
-
-addBlock($tester, $collection);
-
-if(isset($api_executors)) {
+if (isset($api_executors)) {
     addToApiExecution("getViewes", $class = new awtMetrics);
 }
 
 
-if(!$settings->checkIfSettingExists("Show Metrics widgets")) {
+if (!$settings->checkIfSettingExists("Show Metrics widgets")) {
     $settings->createSetting("Show Metrics widgets", "true");
     $settings->fetchSettings();
 }
 
 
-if(function_exists('loadAllWidgets') && defined("DASHBOARD") && $settings->getSettingsValue("Show Metrics widgets") == "true") {
-    $widget = array("name" => "Metrics Widget", "src" => $src."metricsWidget.php");
+if (function_exists('loadAllWidgets') && defined("DASHBOARD") && $settings->getSettingsValue("Show Metrics widgets") == "true") {
+    $widget = array("name" => "Metrics Widget", "src" => $src . "metricsWidget.php");
     pushToWidgets($widget);
 }
 
